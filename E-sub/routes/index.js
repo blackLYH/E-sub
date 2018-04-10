d644ba1cb0eb15a75aca61d7287cf34068a6e5fb
RemiliaDevil <remilia_powercliff@hotmail.com>
2018-04-10 21:15:08 +0800

    fix bug
    fix bug from afternoon now this version is correct

--- E-sub/routes/index.js
+++ E-sub/routes/index.js
@@ -14,11 +14,31 @@ router.get('/login', function(req, res, next) {
 router.post('/login/test', function(req, res, next) {
 
 
+
+    console.log("boom");
+
     var account=req.body["Account"];
     var password_outside=req.body["Password"];
 
     console.log(account);
     console.log(password_outside);
+
+    var connection = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : '123456',
+        database : 'esub'
+    });
+
+    connection.connect();
+
+    connection.query('select password from account where account=?',account, function (error, results) {
+        if (error) throw error;
+        console.log('password: ', results[0].password);
+    });
+
+
+
 
     
 
