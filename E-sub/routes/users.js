c6617391e86d467d041ec630b4bcfcf35f9036b0
RemiliaDevil <remilia_powercliff@hotmail.com>
2018-04-17 15:24:09 +0800

    function:find back password
    if you forget your password,you can find your account back.

--- E-sub/routes/users.js
+++ E-sub/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var mysql   = require('mysql');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -7,11 +8,121 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/getForget_change', function(req, res, next) {
-    res.render('getForget', { title: 'Express' });
+
+
+    var account=req.body["forget_account"];
+    var password=req.body["forget_password"];
+    console.log(account);
+    console.log(password);
+
+    var connection = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : '123456',
+        database : 'esub'
+    });
+
+    connection.connect();
+
+    var  sql = 'update account set password= ? where account= ? ';
+    var piss=[password,account];
+    console.log(sql);
+    connection.query(sql,piss,function (err, result) {
+        if(err){
+            console.log('[SELECT ERROR] - ',err.message);
+            return;
+        }
+        else {
+
+           console.log("asdf");
+
+            res.json({success:'password correct'});
+
+            return;
+        }
+
+
+    });
+
+    connection.end();
+
 });
 
 router.post('/getForget_account', function(req, res, next) {
-    res.render('getForget', { title: 'Express' });
+
+    console.log("test for forget");
+    var account_outside=req.body["account"];
+
+    console.log(account_outside);
+  //  res.render('getForget', { title: 'Express' });
+  //  var account_outside=req.body("account");
+  //  console.log(account_outside);
+
+    var password="haha";
+
+    var connection = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : '123456',
+        database : 'esub'
+    });
+
+    connection.connect();
+
+    var _getUser = function(name, callback) {
+
+        var sql = "SELECT password FROM  account  WHERE account=?";
+
+        connection.query(sql, account_outside, function (err, results) {
+            if (!err) {
+                var res = hasUser(results)
+                callback(res);
+
+            } else {
+                callback(error());
+            }
+        });
+
+        function hasUser(results) {
+            // console.log(results);
+            if (results.length == 0) {
+                return "not exist";
+            }
+            else {
+                return results[0];
+            }
+        }
+        function error() {
+            return "database error";
+        }
+    }
+
+    var getUser = function(name, callback){
+        return _getUser(name, callback);
+    }
+
+    getUser(password, function(data) {
+
+
+        var User = data;
+
+        console.log(User.password);
+
+        if(User.password==undefined){
+            console.log("食屎啦你没注册的");
+            res.status(404);
+            res.json({error:'NO account'});
+            return;
+        }
+        else {
+            console.log("账号存在");
+        }
+
+
+    });
+
+
+
 });
 
 
