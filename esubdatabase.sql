c1b232fdbfa1b9241eaa263b745ad22e3f4ac0bc
RemiliaDevil <remilia_powercliff@hotmail.com>
2018-05-06 12:29:55 +0800

    datebase update

--- esubdatabase.sql
+++ esubdatabase.sql
@@ -1,23 +1,32 @@
 /*
-用户类属性：
-用户id
-用户名
-用户密码
-用户手机
-用户邮箱
-验证码
-验证码时间
-验证码是否激活
-用户头像
+Navicat MySQL Data Transfer
+
+Source Server         : HXM
+Source Server Version : 50639
+Source Host           : 45.76.169.253:3306
+Source Database       : esub
+
+Target Server Type    : MYSQL
+Target Server Version : 50639
+File Encoding         : 65001
+
+Date: 2018-05-06 12:28:26
 */
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for account
+-- ----------------------------
 DROP TABLE IF EXISTS `account`;
 CREATE TABLE `account` (
   `account` varchar(20) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL COMMENT 'ONLY LOW_CASE FORMAT',
+  `password_for_developer` varchar(255) DEFAULT NULL,
   `pwd_strength` varchar(50) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
   `mail` varchar(50) DEFAULT NULL,
-  `image` varchar(50) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
   `sex` varchar(255) DEFAULT NULL,
   `birthday_year` varchar(255) DEFAULT NULL,
   `birthday_month` varchar(255) DEFAULT NULL,
@@ -25,20 +34,58 @@ CREATE TABLE `account` (
   `identitycode` varchar(8) DEFAULT NULL,
   `identitytime` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `identity` int(1) DEFAULT '0',
-  `password_for_developer` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`account`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- ----------------------------
+-- Table structure for ecode
+-- ----------------------------
+DROP TABLE IF EXISTS `ecode`;
+CREATE TABLE `ecode` (
+  `email` varchar(50) DEFAULT NULL,
+  `code` varchar(10) DEFAULT NULL,
+  `alived` tinyint(1) DEFAULT NULL,
+  `time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- ----------------------------
+-- Table structure for personal
+-- ----------------------------
+DROP TABLE IF EXISTS `personal`;
+CREATE TABLE `personal` (
+  `account` varchar(255) NOT NULL,
+  `remain_money` varchar(255) DEFAULT NULL,
+  `paid_subtitle` varchar(1000) DEFAULT NULL COMMENT 'record subtitles'' IDs which the user already bought.\r\nformat:\r\nsubtitle01+salt+subtitle02+salt.......',
+  `premium` int(2) NOT NULL COMMENT 'Have you paid enough?',
+  `premium_uptodate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `sell_subtitle` varchar(1000) DEFAULT NULL COMMENT 'subtitles the user selling.\r\nformat:\r\nsubtitle01+salt+subtitle02+salt.....',
+  PRIMARY KEY (`account`),
+  CONSTRAINT `account` FOREIGN KEY (`account`) REFERENCES `account` (`account`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*
-视频id
-所属用户id
-视频名字
-资源地址
-原视频id
-语言
-*/
+-- ----------------------------
+-- Table structure for subtitle
+-- ----------------------------
+DROP TABLE IF EXISTS `subtitle`;
+CREATE TABLE `subtitle` (
+  `subtitle_ID` int(10) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) NOT NULL,
+  `video_ID` int(10) DEFAULT NULL,
+  `publisher` varchar(255) DEFAULT NULL,
+  `label` varchar(255) DEFAULT NULL,
+  `languate` varchar(100) DEFAULT NULL,
+  `rank` float(2,0) DEFAULT NULL COMMENT 'you can rank the subtitle after bought it.\r\nlike DouBan rank\r\n1~10 ranks,ony interger,the total result can be float.',
+  `instruction` varchar(500) DEFAULT NULL,
+  `price` varchar(50) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `download_count` int(5) DEFAULT NULL,
+  PRIMARY KEY (`subtitle_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for video
+-- ----------------------------
+DROP TABLE IF EXISTS `video`;
 CREATE TABLE `video` (
   `vid` int(11) NOT NULL AUTO_INCREMENT,
   `userid` int(11) DEFAULT NULL,
@@ -49,57 +96,3 @@ CREATE TABLE `video` (
   PRIMARY KEY (`vid`),
   KEY `abc` (`userid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*
-DELIMITER $$
-CREATE PROCEDURE Mypro(in idd int)
-BEGIN
-update user SET identity= 1 WHERE id = idd;
-END;
-$$
-DELIMITER ;
-drop event if exists e_test;
-create event e_test
-on schedule at date_add(now(),interval 1 minute)
-on completion preserve
-do call Mypro();
-alter event test_event on completion preserve enable;
-delimiter **
-CREATE TRIGGER demo after update 
-on user for each row
-begin 
-alter event test_event on completion preserve enable;
-end;
-**
-delimiter ;
-*/
-
--- ----------------------------
--- Table structure for personal
--- ----------------------------
-DROP TABLE IF EXISTS `personal`;
-CREATE TABLE `personal` (
-  `account` varchar(255) NOT NULL,
-  `remain_money` varchar(255) DEFAULT NULL,
-  `payment_diary` varchar(1000) DEFAULT NULL COMMENT 'records a user''s payment history:\r\nformat:\r\nTime+salt+payment_account+salt+patment_source\r\n',
-  `paid_subtitle` varchar(1000) DEFAULT NULL COMMENT 'record subtitles'' IDs which the user already bought.\r\nformat:\r\nsubtitle01+salt+subtitle02+salt.......',
-  `premium` int(2) NOT NULL COMMENT 'Have you paid enough?',
-  `premium_uptodate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `sell_subtitle` varchar(1000) DEFAULT NULL COMMENT 'subtitles the user selling.\r\nformat:\r\nsubtitle01+salt+subtitle02+salt.....',
-  PRIMARY KEY (`account`),
-  CONSTRAINT `account` FOREIGN KEY (`account`) REFERENCES `account` (`account`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-DROP TABLE IF EXISTS `subtitle`;
-CREATE TABLE `subtitle` (
-  `subtitle_ID` int(10) NOT NULL AUTO_INCREMENT,
-  `owner` varchar(255) NOT NULL,
-  `label` varchar(255) DEFAULT NULL,
-  `instruction` varchar(500) DEFAULT NULL,
-  `price` varchar(50) NOT NULL,
-  `isPublic` int(2) NOT NULL,
-  `purchaser_dairy` varchar(20000) DEFAULT NULL COMMENT 'format:\r\npurchaser01_ID+salt+purchaser02_ID+salt.....',
-  PRIMARY KEY (`subtitle_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
