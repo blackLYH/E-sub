c13df9a3f90140262d66e652e1a5c690cf251b68
lyc <3212668379@qq.com>
2018-05-14 20:35:30 +0800

    member_center is ok except the time from hayi

--- E-sub/public/javascripts/member_center.js
+++ E-sub/public/javascripts/member_center.js
@@ -80,9 +80,24 @@ $.ajax({
         var login_info=data["login_info"];    
         console.log(login_info);
 
-        var login_info_one;
-        login_info_one=login_info.split("****");
-        
+
+        var login_info_one=login_info.split("****");
+        var login_info_more1=login_info_one[0].split("$$$");
+        var login_info_more2=login_info_one[1].split("$$$");
+        console.log(login_info_one);
+        console.log(login_info_more1);
+        console.log(login_info_more2);
+        $("#t11").append("哈衣没给");
+        $("#t12").append(login_info_more1[0]);
+        $("#t14").append(login_info_more1[1]);
+        $("#t13").append(login_info_more1[2]);
+        $("#t21").append("哈衣没给");
+        $("#t22").append(login_info_more2[0]);
+        $("#t24").append(login_info_more2[1]);
+        $("#t23").append(login_info_more2[2]);
+
+
+
 
 
         imageold = userimage;
